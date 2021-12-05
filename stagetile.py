53f30dc755213badab5047a852da3f092a073bfa
dp2421 <80231209+dp2421@users.noreply.github.com>
2021-12-05 15:56:16 +0900

    맵 생성

--- stagetile.py
+++ stagetile.py
@@ -5,9 +5,24 @@ import game_framework
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0/TIME_PER_ACTION
 FRAMES_PER_ACTION = 4
+first_blocks = [2, 0, 0, 0, 1, 2, 1, 2, 1]
+for i in range(60):
+    first_blocks.append(0)
+
+first_blocks.extend([1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 1, 1, 1, 2, 0, 0, 0, 0, 0, 1,
+    2, 0, 0, 0, 0, 2, 0, 0, 2, 0,
+    0, 2, 0, 0, 0, 0, 0, 1,
+    0, 0, 1, 1, 1, 0, 0, 0, 1, 1])
+
+for i in range(60):
+    first_blocks.append(0)
+
+first_blocks.extend([1, 1, 2, 1])
 
 class StageGround:
     def __init__ (self):
+        self.blockimage = load_image('resource/main_ground.png')
         self.qimage = load_image('resource/questiontile.png')
         self.image = load_image('resource/stagetile.png')
         self.underimage = load_image('resource/stagegrass.png')
@@ -16,6 +31,7 @@ class StageGround:
         self.w = self.image.w
         self.h = self.image.h
         self.x = 34
+        self.by=150
         self.cx =0
         self.frame = 0
 
@@ -39,7 +55,16 @@ class StageGround:
             self.image.draw(self.x * i + self.cx,0)
             self.image.draw(self.x * i + self.cx, 23)
             self.underimage.draw(self.x * i + self.cx, 55)
-        self.qimage.clip_draw(int(self.frame) * 34, 0, 35, 36, 400+self.cx, 150)
+
+        for index, value in enumerate(first_blocks):
+            if value == 0:
+                pass
+            elif value == 1:
+                self.blockimage.draw(680+index*30 + self.cx, self.by)
+                pass
+            elif value == 2:
+                self.qimage.clip_draw(int(self.frame) * 34, 0, 35, 36, 680+index*30 + self.cx, self.by)
+                pass
         pass
 
     def update(self):
